075b157043ce247fc64001b4df29ec5403c5fd01
lijinhai255 <18854109500@163.com>
2025-09-07 20:43:02 +0800

    fix:暂时移除实验性功能，等稳定后再启用

--- next.config.ts
+++ next.config.ts
@@ -20,10 +20,6 @@ const nextConfig: NextConfig = {
       },
     ]
   } ,
-  experimental: {
-    ppr:"incremental",
-    after:true
-  },
   devIndicators: {
     appIsrStatus:true,
     buildActivity:true,
